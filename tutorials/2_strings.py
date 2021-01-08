43fc0ef8f9437162644403609e12c15d768924c3
Enes Ozmen <72456817+ozmenes@users.noreply.github.com>
2021-01-08 23:09:15 +0100

    python tutorials

---
+++ tutorials/2_strings.py
@@ -0,0 +1,35 @@
+# Strings AND print
+
+# \ kacis dizisi onune geldigi karakterin anlamini degisitrioyr 
+
+x = 'Hello Python'
+y = "Hello Java"
+z = 'Hello\'s dart'# dikkat
+
+print(x, y, z)
+
+print(x + x + x)
+
+print(y * 3)
+
+print("Python",3)
+
+print("Python" + str(2021))
+
+print(int("13") + 5)
+
+print(float(2.5) + 5)
+
+x = 'Hello Python'
+
+print(x[0])
+
+print(x[5:])
+
+print(x[0:5])
+
+print(x[-1])
+
+print(len(x))
+
+print(len("Python"))
---
+++ tutorials/3_variable_types.py
@@ -0,0 +1,24 @@
+#Assigning Values to Variables
+
+x = 10
+
+print(type(x))
+
+y = 2.6
+
+print(type(y))
+
+c = "Python"
+
+print(type(c))
+
+l = [1,2,3,4]
+t = (1,2,3,4)
+print(type(l),type(t))
+
+print(type({"elma":"portakal"}))
+
+a = "Hello"
+b = "Python"
+r = a+b
+print(r) 
