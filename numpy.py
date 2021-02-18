16285b8dd596b349fff9afd08a3eb8f4b57afa1e
Enes Ozmen <72456817+ozmenes@users.noreply.github.com>
2021-02-19 00:04:08 +0100

    Update and rename namba to numpy.py

---
+++ numpy.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+#Access Array Elements
+# arr = np.array([[1, 2, 3], [4, 5, 6]]) 2-D arrays
+#arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])# 3-D arrays
+arr = np.array([1, 2, 3, 4], ndmin=5)
+print(arr)
+print('number of dimensions :', arr.ndim)
+b = np.array([1, 2, 3, 4, 5])
+print(b.ndim)
+
+
+
+print(b[3])
